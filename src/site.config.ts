d8595f1be96bdd454362000506b2f0065efc6c07
MaxK9999 <maxkoltypin@gmail.com>
2025-11-21 22:26:09 +0100

    giscus modification

--- src/site.config.ts
+++ src/site.config.ts
@@ -2,7 +2,7 @@ import type { SiteConfig } from '~/types'
 
 const config: SiteConfig = {
   // Absolute URL to the root of your published site, used for generating links and sitemaps.
-  site: 'https://maxsec.com', 
+  site: 'https://maxk9999.vercel.app/posts/imagery', 
   // The name of your site, used in the title and for SEO.
   title: 'MAXSEC',
   // The description of your site, used for SEO and RSS feed.
